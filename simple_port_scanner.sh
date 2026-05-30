962e1f881bc9ccf107d72c9650d0448e14ba7cb3
DarioJr02 <dariocmia@gmail.com>
2026-05-30 14:57:18 +0200

    b<main>: Poblate p_<> vars logic.

--- simple_port_scanner.sh
+++ simple_port_scanner.sh
@@ -1,33 +1,34 @@
 #! /bin/zsh
 
 # Variables
-declare -A variables
+#declare -A variables
 
-variables[p_wk_begin]=0
-variables[p_wk_end]=1023
+#variables[p_wk_begin]=0
+#variables[p_wk_end]=1023
 
-variables[p_r_begin]=1024
-variables[p_r_end]=49151
+#variables[p_r_begin]=1024
+#variables[p_r_end]=49151
 
-variables[p_dy_begin]=49152
-variables[p_dy_end]=65535
+#variables[p_dy_begin]=49152
+#variables[p_dy_end]=65535
 
 # O hacer 3 arrays y poblarlos.
 
 n=0
-p_wd=()
+p_wk=()
 p_r=()
 p_dy=()
 
 # Functions:
 # - To poblate the variables arrays fnc(start, end)
 
+# ------------------------------------------------
 until [ $n -ge 1024 ]; do
-  printf "index = ${p_w}"
+  p_wk+=${n}
   ((n++))
-    p_wd+=${n}
+done
 
-printf "Loop poblado"
-
-for i in p_wd; do
-    printf "${i}"
+if [ ${#p_wk[@]} -eq 1024 ]; then
+  printf "\nEl tamaño de p_wd = ${#p_wk[@]}\n\n"
+fi
+# ------------------------------------------------
