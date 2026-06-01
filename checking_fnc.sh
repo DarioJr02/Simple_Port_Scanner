97c5e344618afa4dcc42ec6018aa6935955f8565
DarioJr02 <dariocmia@gmail.com>
2026-06-01 13:14:19 +0200

    f<checking_fnc.sh>: tr 'A-Z' -> 'a-z'

--- checking_fnc.sh
+++ checking_fnc.sh
@@ -1,25 +1,25 @@
 #! /bin/bash
 
 determine_port_range() {
-  local -n L_RANGE_TO_CHECK=$1
-  if [[ ${#L_RANGE_TO_CHECK[@]} -eq $((${#L_RANGE_TO_CHECK[@]} + 0)) ]]; then
-    printf "PORTS %s: %s [INITIALIZED]\n\n" "$2" "${#L_RANGE_TO_CHECK[@]}"
+  local -n l_range_to_check=$1
+  if [[ ${#l_range_to_check[@]} -eq $((${#l_range_to_check[@]} + 0)) ]]; then
+    printf "ports %s: %s [initialized]\n\n" "$2" "${#l_range_to_check[@]}"
   fi
 
-  if [[ ${#L_RANGE_TO_CHECK[@]} -eq $((${#L_RANGE_TO_CHECK[@]} + 1024)) ]]; then
-    printf "PORTS %s: %s [INITIALIZED]\n\n" "$2" "${#L_RANGE_TO_CHECK[@]}"
+  if [[ ${#l_range_to_check[@]} -eq $((${#l_range_to_check[@]} + 1024)) ]]; then
+    printf "ports %s: %s [initialized]\n\n" "$2" "${#l_range_to_check[@]}"
   fi
 
-  if [[ ${#L_RANGE_TO_CHECK[@]} -eq $((${#L_RANGE_TO_CHECK[@]} + 16385)) ]]; then
-    printf "PORTS %s: %s [INITIALIZED]\n\n" "$2" "${#L_RANGE_TO_CHECK[@]}"
+  if [[ ${#l_range_to_check[@]} -eq $((${#l_range_to_check[@]} + 16385)) ]]; then
+    printf "ports %s: %s [initialized]\n\n" "$2" "${#l_range_to_check[@]}"
   fi
 
-  # Estos outputs deberían ir a log.txt
+  # estos outputs deberían ir a log.txt
 
 }
 
 check_array_size() {
-  local -n L_A_TO_CHECK=$1
-  #local SIZE+=${L_A_TO_CHECK[*]}
-  printf "The Array size is %s.\n\n" "${#L_A_TO_CHECK[@]}"
+  local -n l_a_to_check=$1
+  #local size+=${l_a_to_check[*]}
+  printf "Port range size is %s.\n\n" "${#l_a_to_check[@]}"
 }
